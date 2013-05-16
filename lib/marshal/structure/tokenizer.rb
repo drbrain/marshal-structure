5356975a217bf94cb8cdbd4174261e7ae3841e59
Eric Hodel <drbrain@segment7.net>
2013-05-15 22:02:04 -0700

    Move tokenization to Tokenizer class

---
+++ lib/marshal/structure/tokenizer.rb
@@ -0,0 +1,420 @@
+class Marshal::Structure::Tokenizer
+
+  ##
+  # nil type prefix
+
+  TYPE_NIL = '0'
+
+  ##
+  # true type prefix
+
+  TYPE_TRUE = 'T'
+
+  ##
+  # false type prefix
+
+  TYPE_FALSE = 'F'
+
+  ##
+  # Fixnum type prefix
+
+  TYPE_FIXNUM = 'i'
+
+  ##
+  # An object that has been extended with a module
+
+  TYPE_EXTENDED = 'e'
+
+  ##
+  # A subclass of a built-in type
+
+  TYPE_UCLASS = 'C'
+
+  ##
+  # A ruby Object
+
+  TYPE_OBJECT = 'o'
+
+  ##
+  # A wrapped C pointer
+
+  TYPE_DATA = 'd'
+
+  ##
+  # An object saved with _dump
+
+  TYPE_USERDEF = 'u'
+
+  ##
+  # An object saved with marshal_dump
+
+  TYPE_USRMARSHAL = 'U'
+
+  ##
+  # A Float
+
+  TYPE_FLOAT = 'f'
+
+  ##
+  # A Bignum
+
+  TYPE_BIGNUM = 'l'
+
+  ##
+  # A String
+
+  TYPE_STRING = '"'
+
+  ##
+  # A Regexp
+
+  TYPE_REGEXP = '/'
+
+  ##
+  # An Array
+
+  TYPE_ARRAY = '['
+
+  ##
+  # A Hash
+
+  TYPE_HASH = '{'
+
+  ##
+  # A Hash with a default value (not proc)
+
+  TYPE_HASH_DEF = '}'
+
+  ##
+  # A Struct
+
+  TYPE_STRUCT = 'S'
+
+  ##
+  # An old-style Module (reference, not content)
+  #
+  # I'm not sure what makes this old.  The byte stream is identical to
+  # TYPE_MODULE
+
+  TYPE_MODULE_OLD = 'M'
+
+  ##
+  # A class (reference, not content)
+
+  TYPE_CLASS = 'c'
+
+  ##
+  # A module (reference, not content)
+
+  TYPE_MODULE = 'm'
+
+  ##
+  # A Symbol
+
+  TYPE_SYMBOL = ':'
+
+  ##
+  # A reference to a previously Symbol
+
+  TYPE_SYMLINK = ';'
+
+  ##
+  # Instance variables for a following object
+
+  TYPE_IVAR = 'I'
+
+  ##
+  # A reference to a previously-stored Object
+
+  TYPE_LINK = '@'
+
+  TYPE_MAP = Hash.new do |_, type| # :nodoc:
+    raise Error, "unknown type #{type.inspect}"
+  end
+
+  TYPE_MAP[TYPE_ARRAY]      = :array
+  TYPE_MAP[TYPE_BIGNUM]     = :bignum
+  TYPE_MAP[TYPE_CLASS]      = :class
+  TYPE_MAP[TYPE_DATA]       = :data
+  TYPE_MAP[TYPE_EXTENDED]   = :extended
+  TYPE_MAP[TYPE_FALSE]      = :false
+  TYPE_MAP[TYPE_FIXNUM]     = :fixnum
+  TYPE_MAP[TYPE_FLOAT]      = :float
+  TYPE_MAP[TYPE_HASH]       = :hash
+  TYPE_MAP[TYPE_HASH_DEF]   = :hash_default
+  TYPE_MAP[TYPE_IVAR]       = :instance_variables
+  TYPE_MAP[TYPE_LINK]       = :link
+  TYPE_MAP[TYPE_MODULE]     = :module
+  TYPE_MAP[TYPE_MODULE_OLD] = :module_old
+  TYPE_MAP[TYPE_NIL]        = :nil
+  TYPE_MAP[TYPE_OBJECT]     = :object
+  TYPE_MAP[TYPE_REGEXP]     = :regexp
+  TYPE_MAP[TYPE_STRING]     = :string
+  TYPE_MAP[TYPE_STRUCT]     = :struct
+  TYPE_MAP[TYPE_SYMBOL]     = :symbol
+  TYPE_MAP[TYPE_SYMLINK]    = :symbol_link
+  TYPE_MAP[TYPE_TRUE]       = :true
+  TYPE_MAP[TYPE_UCLASS]     = :user_class
+  TYPE_MAP[TYPE_USERDEF]    = :user_defined
+  TYPE_MAP[TYPE_USRMARSHAL] = :user_marshal
+
+  def initialize stream
+    @byte_array = stream.bytes.to_a
+    @consumed   = 2
+    @state      = [:any]
+    @stream     = stream
+    @stream.force_encoding Encoding::BINARY
+  end
+
+  ##
+  # Decodes a stored Fixnum
+
+  def construct_integer
+    c = consume_byte
+
+    return 0 if c == 0
+
+    # convert to signed integer
+    c = (c ^ 0x80) - 0x80
+
+    if c > 0 then
+      return c - 5 if 4 < c
+
+      x = 0
+
+      c.times do |i|
+        x |= consume_byte << (8 * i)
+      end
+      
+      x
+    else
+      return c + 5 if c < -4
+
+      x = -1
+
+      (-c).times do |i|
+        factor = 8 * i
+        x &= ~(0xff << factor)
+        x |= consume_byte << factor
+      end
+
+      x
+    end
+  end
+
+  ##
+  # Consumes +bytes+ from the marshal stream
+
+  def consume bytes
+    raise Marshal::Structure::EndOfMarshal.new(@consumed, bytes) if
+      @consumed + bytes > @stream.size
+
+    data = @stream[@consumed, bytes]
+    @consumed += bytes
+    data
+  end
+
+  ##
+  # Consumes +count+ bytes from the marshal stream as an Array of bytes
+
+  def consume_bytes count
+    consume(count).bytes.to_a
+  end
+
+  ##
+  # Consumes one byte from the marshal stream
+
+  def consume_byte
+    raise Marshal::Structure::EndOfMarshal.new(@consumed, 1) if
+      @consumed >= @byte_array.size
+
+    data = @byte_array[@consumed]
+    @consumed += 1
+
+    data
+  end
+
+  ##
+  # Consumes one byte from the marshal stream and returns a character
+
+  def consume_character
+    consume_byte.chr
+  end
+
+  ##
+  # Consumes a sequence of bytes from the marshal stream based on the next
+  # integer
+
+  def get_byte_sequence
+    size = construct_integer
+    consume size
+  end
+
+  ##
+  # Attempts to retrieve the next token from the stream.  You may need to call
+  # next_token twice to receive a token as the current token may be
+  # incomplete.
+
+  def next_token # :nodoc:
+    current_state = @state.pop
+
+    case current_state
+    when :any                         then tokenize_any
+    when :array                       then tokenize_array
+    when :bignum                      then tokenize_bignum
+    when :byte                        then consume_byte
+    when :bytes,
+         :class, :module, :module_old,
+         :float, :string, :symbol     then get_byte_sequence
+    when :data                        then tokenize_data
+    when :extended                    then tokenize_extended
+    when :fixnum, :link, :symbol_link then construct_integer
+    when :hash, :pairs                then tokenize_pairs
+    when :hash_default                then tokenize_hash_default
+    when :instance_variables          then tokenize_instance_variables
+    when :object                      then tokenize_object
+    when :regexp                      then tokenize_regexp
+    when :struct                      then tokenize_struct
+    when :sym                         then tokenize_sym
+    when :user_class                  then tokenize_user_class
+    when :user_defined                then tokenize_user_defined
+    when :user_marshal                then tokenize_user_marshal
+    else
+      raise Error, "bug: unknown state #{current_state.inspect}"
+    end
+  end
+
+  ##
+  # Returns an Enumerator that will tokenize the Marshal stream.
+
+  def tokens
+    Enumerator.new do |yielder|
+      until @state.empty? do
+        token = next_token
+
+        yielder << token if token
+      end
+    end
+  end
+
+  def tokenize_any # :nodoc:
+    item_type = TYPE_MAP[consume_character]
+
+    @state.push item_type unless [:nil, :true, :false].include? item_type
+
+    item_type
+  end
+
+  def tokenize_array # :nodoc:
+    size = construct_integer
+
+    @state.concat Array.new(size, :any)
+
+    size
+  end
+
+  def tokenize_bignum # :nodoc:
+    sign = consume_byte == 45 ? -1 : 1
+    size = construct_integer * 2
+
+    result = 0
+
+    data = consume_bytes size
+
+    data.each_with_index do |data, exp|
+      result += (data * 2**(exp*8))
+    end
+
+    sign * result
+  end
+
+  def tokenize_data # :nodoc:
+    @state.push :any
+    @state.push :sym
+
+    next_token
+  end
+
+  alias tokenize_extended tokenize_data # :nodoc:
+
+  def tokenize_hash_default # :nodoc:
+    size = construct_integer
+
+    @state.push :any
+    @state.push size * 2 if size > 0
+
+    size
+  end
+
+  def tokenize_instance_variables # :nodoc:
+    @state.push :pairs
+    @state.push :any
+
+    next_token
+  end
+
+  ##
+  # For multipart objects like arrays and hashes a count of items is pushed
+  # onto the stack.  This method re-pushes an :any onto the stack until the
+  # correct number of tokens have been created from the stream.
+
+  def tokenize_next_any current_state # :nodoc:
+    next_state = current_state - 1
+    @state.push next_state if current_state > 0
+    @state.push :any
+
+    next_token
+  end
+
+  def tokenize_object # :nodoc:
+    @state.push :fixnum
+    @state.push :sym
+
+    next_token
+  end
+
+  def tokenize_pairs # :nodoc:
+    size = construct_integer
+
+    @state.concat Array.new(size * 2, :any)
+
+    size
+  end
+
+  def tokenize_regexp # :nodoc:
+    @state.push :byte
+
+    get_byte_sequence
+  end
+
+  def tokenize_struct # :nodoc:
+    @state.push :pairs
+    @state.push :sym
+
+    next_token
+  end
+
+  def tokenize_sym # :nodoc:
+    item_type = TYPE_MAP[consume_character]
+
+    raise Error, "expected symbol type, got #{item_type.inspect}" unless
+      [:symbol, :symbol_link].include? item_type
+
+    @state.push item_type
+
+    item_type
+  end
+
+  alias tokenize_user_class tokenize_data # :nodoc:
+
+  def tokenize_user_defined # :nodoc:
+    @state.push :bytes
+    @state.push :sym
+
+    next_token
+  end
+
+  alias tokenize_user_marshal tokenize_data # :nodoc:
+
+end
+
---
+++ test/test_marshal_structure_tokenizer.rb
@@ -0,0 +1,317 @@
+require 'minitest/autorun'
+require 'marshal/structure'
+require 'pp'
+
+class TestMarshalStructureTokenizer < MiniTest::Unit::TestCase
+
+  def mu_pp obj
+    s = ''
+    s = PP.pp obj, s
+    s.chomp
+  end
+
+  def setup
+    @MST = Marshal::Structure::Tokenizer
+  end
+
+  def test_consume
+    ms = @MST.new "\x04\x08\x06M"
+
+    assert_equal "\x06M", ms.consume(2)
+  end
+
+  def test_consume_bytes
+    ms = @MST.new "\x04\x08\x06M"
+
+    assert_equal [6, 77], ms.consume_bytes(2)
+
+    e = assert_raises Marshal::Structure::EndOfMarshal do
+      ms.consume_bytes 3
+    end
+
+    assert_equal 4, e.consumed
+    assert_equal 3, e.requested
+  end
+
+  def test_consume_byte
+    ms = @MST.new "\x04\x08M"
+
+    assert_equal 77, ms.consume_byte
+
+    e = assert_raises Marshal::Structure::EndOfMarshal do
+      ms.consume_byte
+    end
+
+    assert_equal 3, e.consumed
+    assert_equal 1, e.requested
+  end
+
+  def test_consume_character
+    ms = @MST.new "\x04\x08M"
+
+    assert_equal 'M', ms.consume_character
+
+    e = assert_raises Marshal::Structure::EndOfMarshal do
+      ms.consume_character
+    end
+
+    assert_equal 3, e.consumed
+    assert_equal 1, e.requested
+  end
+
+  def test_construct_integer
+    assert_equal        0, @MST.new("\x04\x08\x00").construct_integer
+
+    assert_equal        0, @MST.new("\x04\x08\x01\x00").construct_integer
+    assert_equal        1, @MST.new("\x04\x08\x01\x01").construct_integer
+    assert_equal        0, @MST.new("\x04\x08\x02\x00\x00").construct_integer
+    assert_equal     2<<7, @MST.new("\x04\x08\x02\x00\x01").construct_integer
+    assert_equal        0, @MST.new("\x04\x08\x03\x00\x00\x00").construct_integer
+    assert_equal    2<<15, @MST.new("\x04\x08\x03\x00\x00\x01").construct_integer
+    assert_equal        0,
+                 @MST.new("\x04\x08\x04\x00\x00\x00\x00").construct_integer
+    assert_equal    2<<23,
+                 @MST.new("\x04\x08\x04\x00\x00\x00\x01").construct_integer
+    assert_equal (2<<31) - 1,
+                 @MST.new("\x04\x08\x04\xff\xff\xff\xff").construct_integer
+
+    assert_equal        0, @MST.new("\x04\x08\x05").construct_integer
+    assert_equal        1, @MST.new("\x04\x08\x06").construct_integer
+    assert_equal      122, @MST.new("\x04\x08\x7f").construct_integer
+    assert_equal     -123, @MST.new("\x04\x08\x80").construct_integer
+    assert_equal       -1, @MST.new("\x04\x08\xfa").construct_integer
+    assert_equal        0, @MST.new("\x04\x08\xfb").construct_integer
+
+    assert_equal -(1<<32),
+                 @MST.new("\x04\x08\xfc\x00\x00\x00\x00").construct_integer
+    assert_equal       -1,
+                 @MST.new("\x04\x08\xfc\xff\xff\xff\xff").construct_integer
+    assert_equal -(1<<24), @MST.new("\x04\x08\xfd\x00\x00\x00").construct_integer
+    assert_equal       -1, @MST.new("\x04\x08\xfd\xff\xff\xff").construct_integer
+    assert_equal -(1<<16), @MST.new("\x04\x08\xfe\x00\x00").construct_integer
+    assert_equal       -1, @MST.new("\x04\x08\xfe\xff\xff").construct_integer
+    assert_equal  -(1<<8), @MST.new("\x04\x08\xff\x00").construct_integer
+    assert_equal       -1, @MST.new("\x04\x08\xff\xff").construct_integer
+  end
+
+  def test_get_byte_sequence
+    ms = @MST.new "\x04\x08\x06M"
+
+    assert_equal "M", ms.get_byte_sequence
+  end
+
+  def test_tokens_array
+    ms = @MST.new "\x04\x08[\x07TF"
+
+    assert_equal [:array, 2, :true, :false], ms.tokens.to_a
+  end
+
+  def test_tokens_bignum
+    ms = @MST.new "\x04\x08l-\x07\x00\x00\x00@"
+
+    assert_equal [:bignum, -1073741824], ms.tokens.to_a
+  end
+
+  def test_tokens_class
+    ms = @MST.new "\x04\x08c\x06C"
+
+    assert_equal [:class, 'C'], ms.tokens.to_a
+  end
+
+  def test_tokens_data
+    ms = @MST.new "\x04\bd:\x18OpenSSL::X509::Name[\x00"
+
+    assert_equal [:data, :symbol, 'OpenSSL::X509::Name', :array, 0],
+                 ms.tokens.to_a
+  end
+
+  def test_tokens_extended
+    skip 'todo'
+    ms = @MST.new "\x04\be:\x0FEnumerableo:\vObject\x00"
+
+    expected = [
+      :extended, :symbol, 'Enumerable',
+      :object, :symbol, 'Object', 0
+    ]
+
+    assert_equal expected, ms.tokens.to_a
+  end
+
+  def test_tokens_false
+    ms = @MST.new "\x04\x080"
+
+    assert_equal [:nil], ms.tokens.to_a
+  end
+
+  def test_tokens_fixnum
+    ms = @MST.new "\x04\x08i/"
+
+    assert_equal [:fixnum, 42], ms.tokens.to_a
+  end
+
+  def test_tokens_float
+    ms = @MST.new "\x04\bf\b4.2"
+
+    assert_equal [:float, '4.2'], ms.tokens.to_a
+  end
+
+  def test_tokens_hash
+    ms = @MST.new "\x04\b{\ai\x06i\aTF"
+
+    assert_equal [:hash, 2, :fixnum, 1, :fixnum, 2, :true, :false],
+                 ms.tokens.to_a
+  end
+
+  def test_tokens_hash_default
+    ms = @MST.new "\x04\x08}\x00i\x06"
+
+    assert_equal [:hash_default, 0, :fixnum, 1], ms.tokens.to_a
+  end
+
+  def test_tokens_instance_variables
+    ms = @MST.new "\x04\bI\"\x00\a:\x06ET:\a@xi\a"
+
+    expected = [
+      :instance_variables,
+      :string, '',
+      2, :symbol, 'E', :true, :symbol, '@x', :fixnum, 2,
+    ]
+
+    assert_equal expected, ms.tokens.to_a
+  end
+
+  def test_tokens_link
+    ms = @MST.new "\x04\x08[\x07I\"\x00\x06:\x06ET@\x06"
+
+    expected = [
+      :array, 2,
+        :instance_variables,
+            :string, '',
+          1,
+          :symbol, 'E', :true,
+        :link, 1,
+    ]
+
+    assert_equal expected, ms.tokens.to_a
+  end
+
+  def test_tokens_module
+    ms = @MST.new "\x04\bm\x0FEnumerable"
+
+    assert_equal [:module, 'Enumerable'], ms.tokens.to_a
+  end
+
+  def test_tokens_module_old
+    ms = @MST.new "\x04\bM\x0FEnumerable"
+
+    assert_equal [:module_old, 'Enumerable'], ms.tokens.to_a
+  end
+
+  def test_tokens_object
+    ms = @MST.new "\x04\bo:\vObject\x00"
+
+    assert_equal [:object, :symbol, 'Object', 0], ms.tokens.to_a
+  end
+
+  def test_tokens_regexp
+    ms = @MST.new "\x04\bI/\x06x\x01\x06:\x06EF"
+
+    expected = [
+      :instance_variables,
+          :regexp, 'x', 1,
+        1, :symbol, 'E', :false,
+    ]
+
+    assert_equal expected, ms.tokens.to_a
+  end
+
+  def test_tokens_string
+    ms = @MST.new "\x04\b\"\x06x"
+
+    assert_equal [:string, 'x'], ms.tokens.to_a
+  end
+
+  def test_tokens_struct
+    ms = @MST.new "\x04\x08S:\x06S\x06:\x06ai\x08"
+
+    expected = [
+      :struct, :symbol, 'S', 1, :symbol, 'a', :fixnum, 3
+    ]
+
+    assert_equal expected, ms.tokens.to_a
+  end
+
+  def test_tokens_symbol
+    ms = @MST.new "\x04\x08:\x06S"
+
+    expected = [
+      :symbol, 'S'
+    ]
+
+    assert_equal expected, ms.tokens.to_a
+  end
+
+  def test_tokens_symbol_link
+    ms = @MST.new "\x04\b[\a:\x06s;\x00"
+
+    expected = [
+      :array, 2, :symbol, 's', :symbol_link, 0,
+    ]
+
+    assert_equal expected, ms.tokens.to_a
+  end
+
+  def test_tokens_too_short
+    ms = @MST.new "\x04\x08"
+
+    assert_raises Marshal::Structure::EndOfMarshal do
+      ms.tokens.to_a
+    end
+  end
+
+  def test_tokens_true
+    ms = @MST.new "\x04\x08T"
+
+    assert_equal [:true], ms.tokens.to_a
+  end
+
+  def test_tokens_user_defined
+    ms = @MST.new "\x04\bIu:\tTime\r\xE7Q\x1C\x80\xA8\xC3\x83\xE5\a" \
+                 ":\voffseti\xFE\x90\x9D:\tzoneI\"\bPDT\x06:\x06ET"
+
+    timeval = "\xE7Q\x1C\x80\xA8\xC3\x83\xE5"
+    timeval.force_encoding Encoding::BINARY
+
+    expected = [
+      :instance_variables,
+          :user_defined, :symbol, 'Time', timeval,
+        2,
+          :symbol, 'offset', :fixnum, -25200,
+          :symbol, 'zone',
+            :instance_variables,
+                :string, 'PDT',
+              1, :symbol, 'E', :true,
+    ]
+
+    assert_equal expected, ms.tokens.to_a
+  end
+
+  def test_tokens_user_marshal
+    ms = @MST.new "\x04\bU:\tDate[\vi\x00i\x03l{%i\x00i\x00i\x00f\f2299161"
+
+    expected = [
+      :user_marshal, :symbol, 'Date',
+        :array, 6,
+          :fixnum, 0,
+          :fixnum, 2456428,
+          :fixnum, 0,
+          :fixnum, 0,
+          :fixnum, 0,
+          :float, '2299161',
+    ]
+
+    assert_equal expected, ms.tokens.to_a
+  end
+
+end
+
